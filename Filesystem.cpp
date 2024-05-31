7e766dc0513ac291c2b39fe55fec9cb21b7cdab2
Daru <ozziewei@gmail.com>
2024-06-01 00:09:06 +0800

    implements simple filesystem utilities

---
+++ Filesystem.cpp
@@ -0,0 +1,23 @@
+#include "Utility/Filesystem.h"
+#include "spdlog/spdlog.h"
+#include <filesystem>
+
+namespace utility {
+void createDirectory(const std::string &path) {
+  const std::filesystem::path p{path};
+
+  if (!std::filesystem::exists(p))
+    std::filesystem::create_directories(p);
+}
+
+bool removePath(const std::string &path) {
+  const std::filesystem::path p{path};
+
+  if (!std::filesystem::exists(p)) {
+    spdlog::warn("cannot remove a none-existed path {}", path);
+    return false;
+  }
+
+  return std::filesystem::remove(p);
+}
+} // namespace utility
