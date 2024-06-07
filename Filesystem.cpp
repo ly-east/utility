6e9bac435e53c0100f81192f627a90887da302f3
shin <ozziewei@gmail.com>
2024-06-07 22:20:00 +0800

    getExecutableFilePath partially implemented

--- Filesystem.cpp
+++ Filesystem.cpp
@@ -10,6 +10,23 @@ void createDirectory(const std::string &path) {
     std::filesystem::create_directories(p);
 }
 
+std::string getExecutableFilePath(const std::string &name) {
+  std::filesystem::path p{std::filesystem::current_path()};
+  p.append(name);
+  if (!p.has_extension())
+    p.replace_extension(".exe");
+
+  // Program at the same path of BBDown is preferred to use
+  if (std::filesystem::exists(p) && std::filesystem::is_regular_file(p))
+    return p.string();
+
+  // TODO: use WHERE command to find program in environment variable
+  //  cmd /c "where ffmpeg"
+  std::string path;
+
+  return path;
+}
+
 bool removePath(const std::string &path) {
   const std::filesystem::path p{path};
 
