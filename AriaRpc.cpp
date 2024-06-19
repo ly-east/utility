b963144dd803a1de01dd75217842accc2729f96a
man <ozziewei@gmail.com>
2024-06-19 20:08:56 +0800

    implements getter methods

--- AriaRpc.cpp
+++ AriaRpc.cpp
@@ -16,19 +16,35 @@ const char *getMethodString(Method method) {
 
   return rpc_method.at(method);
 }
+
+const char *getKeyString(Key key) {
+  static const std::unordered_map<Key, const char *> rpc_key{
+      {Key::JSONRPC, "jsonrpc"},
+      {Key::ID, "id"},
+      {Key::METHOD, "method"},
+      {Key::PARAMS, "params"}};
+
+  return rpc_key.at(key);
+}
+
+const char *getValueString(Value value) {
+  static const std::unordered_map<Value, const char *> rpc_value{
+      {Value::JSONRPC, "2.0"}, {Value::ID, "qwer"}};
+
+  return rpc_value.at(value);
+}
 } // namespace
 
 namespace utility {
 namespace aria {
 
 bool ariaRpc(Method method, const std::string &param, const RpcArgTy &option) {
-
   using nlohmann::json;
 
-  json j = {{"jsonrpc", "2.0"},
-            {"id", "qwer"},
-            {"method", getMethodString(method)},
-            {"params", {{param}}}};
+  json j = {{getKeyString(Key::JSONRPC), getValueString(Value::JSONRPC)},
+            {getKeyString(Key::ID), getValueString(Value::ID)},
+            {getKeyString(Key::METHOD), getMethodString(method)},
+            {getKeyString(Key::PARAMS), {{param}}}};
 
   if (option.size()) {
     json opt;
@@ -36,7 +52,7 @@ bool ariaRpc(Method method, const std::string &param, const RpcArgTy &option) {
     for (const auto &pair : option)
       opt[pair.first] = pair.second;
 
-    j["params"].emplace_back(std::move(opt));
+    j[getKeyString(Key::PARAMS)].emplace_back(std::move(opt));
   }
 
   spdlog::debug("\n{}", j.dump());
