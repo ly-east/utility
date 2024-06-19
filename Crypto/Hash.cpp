a765ddc19a26d21bda402bf01335e36217906822
man <ozziewei@gmail.com>
2024-06-19 20:43:03 +0800

    prepare to migrate crypto methods to Utility

---
+++ Crypto/Hash.cpp
@@ -0,0 +1,21 @@
+#include "Utility/Crypto/Hash.h"
+#include "openssl/md5.h"
+#include "spdlog/spdlog.h"
+#include <iomanip>
+#include <sstream>
+
+namespace utility {
+namespace crypto {
+std::string getMD5(const std::string &str) {
+  unsigned char hash[MD5_DIGEST_LENGTH];
+  MD5((unsigned char *)str.c_str(), str.size(), hash);
+
+  std::stringstream ss;
+  for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
+    ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+
+  auto result{ss.str()};
+  spdlog::debug("MD5: {}", result);
+}
+} // namespace crypto
+} // namespace utility
