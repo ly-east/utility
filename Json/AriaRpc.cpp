b408d3848941b875ab25a1825d8f22a4e1c432ff
Daru <ozziewei@gmail.com>
2024-06-21 17:00:59 +0800

    implements GID parsing method of Aria response

--- Json/AriaRpc.cpp
+++ Json/AriaRpc.cpp
@@ -1,10 +1,14 @@
 #include "Utility/Json/AriaRpc.h"
+#include "Utility/Json/Json.h"
 #include "nlohmann/json.hpp"
 #include "spdlog/spdlog.h"
+#include <exception>
+#include <functional>
 #include <unordered_map>
 
 namespace {
 using namespace utility::aria;
+using RpcRespRecvTy = std::function<bool(const nlohmann::json &)>;
 
 const char *getMethodString(Method method) {
   static const std::unordered_map<Method, const char *> rpc_method{
@@ -33,6 +37,36 @@ const char *getValueString(Value value) {
 
   return rpc_value.at(value);
 }
+
+bool parseRpcResponse(const std::string &response, RpcRespRecvTy func) {
+  auto resp = utility::json::parseJsonString(response);
+  if (!resp)
+    return false;
+
+  nlohmann::json result;
+  const std::string error{"error"};
+  if (!utility::json::getOptionalField(*resp, "result", error, result)) {
+    spdlog::error("parseRpcResponse: getOptionalField failed");
+    return false;
+  }
+
+  if (resp->contains(error)) {
+    int code = 0;
+    std::string message;
+
+    try {
+      code = result["code"].template get<decltype(code)>();
+      message = result["message"].template get<decltype(message)>();
+    } catch (const std::exception &e) {
+      spdlog::error("parseRpcResponse: {}", e.what());
+    }
+
+    spdlog::error("Aria2 returns error({}): {}", code, message);
+    return false;
+  }
+
+  return func(result);
+}
 } // namespace
 
 namespace utility {
@@ -56,6 +90,21 @@ std::string getRpcRequest(Method method, const std::string &param,
   }
 
   return j.dump();
+}
+
+bool getRpcResponseGid(const std::string &response, std::string &gid) {
+  auto extractor = [&gid](const nlohmann::json &result) -> bool {
+    try {
+      gid = result.template get<std::string>();
+    } catch (const std::exception &e) {
+      spdlog::error("getRpcResponseGid failed: {}", e.what());
+      return false;
+    }
+
+    return true;
+  };
+
+  return parseRpcResponse(response, extractor);
 }
 } // namespace aria
 } // namespace utility
