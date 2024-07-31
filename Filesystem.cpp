5d4e01039e94249478badf6e9c5b19c6644858bc
Daru <ozziewei@gmail.com>
2024-07-31 11:54:05 +0800

    update api of filesystem

--- Filesystem.cpp
+++ Filesystem.cpp
@@ -1,17 +1,30 @@
 #include "Utility/Filesystem.h"
-#include "Utility/Process/Launch.h"
 #include "spdlog/spdlog.h"
-#include <filesystem>
 
 namespace utility {
-void createDirectory(const std::string &path) {
-  const std::filesystem::path p{path};
-
+void createDirectory(const std::filesystem::path &p) {
   if (!std::filesystem::exists(p))
     std::filesystem::create_directories(p);
 }
 
-bool removePath(const std::filesystem::path &path) {
+bool remove(const std::filesystem::path &path) {
+  if (!std::filesystem::exists(path)) {
+    spdlog::warn("cannot remove a none-existed path {}", path.string());
+    return false;
+  }
+
+  try {
+    std::filesystem::remove(path);
+  } catch (const std::exception &e) {
+    // spdlog::error("remove: {}. Maybe path indicates a non-empty directory",
+    //               e.what());
+    return false;
+  }
+
+  return true;
+}
+
+bool removeAll(const std::filesystem::path &path) {
   if (!std::filesystem::exists(path)) {
     spdlog::warn("cannot remove a none-existed path {}", path.string());
     return false;
