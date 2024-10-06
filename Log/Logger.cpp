07a4cb742ec96087cc86168bb8b41c3a73060c0c
Daru <ozziewei@gmail.com>
2024-10-06 21:25:57 +0800

    output log file

--- Log/Logger.cpp
+++ Log/Logger.cpp
@@ -1,17 +1,21 @@
 #include "Utility/Log/Logger.h"
-#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/spdlog.h"
+#include <filesystem>
+#include <string>
 
 namespace utility {
 namespace log {
 bool setFileLogger(const std::string &filename) {
-  static bool has_settled = false;
-
-  if (has_settled)
-    return true;
+  std::filesystem::path log_path{std::filesystem::current_path().append("log")};
+  if (!std::filesystem::exists(log_path))
+    std::filesystem::create_directories(log_path);
+  log_path.append(filename);
 
   try {
-    auto logger = spdlog::basic_logger_mt("basic_logger", filename);
+    // create a file rotating logger with 4MB size max and 3 rotated files
+    auto logger = spdlog::rotating_logger_mt("logger", log_path.string(),
+                                             1024 * 1024 * 4, 3);
 
     // [Y-M-D H-M-S.e][thread ID][log level]content
     logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e][%t][%l]%v");
@@ -19,7 +23,6 @@ bool setFileLogger(const std::string &filename) {
     logger->flush_on(spdlog::level::err);
 
     spdlog::set_default_logger(logger);
-    has_settled = true;
   } catch (const spdlog::spdlog_ex &ex) {
     spdlog::error("Log init failed: {}", ex.what());
     return false;
