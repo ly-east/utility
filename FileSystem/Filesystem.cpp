bbae2e2fec489ab83c3e4c12b9937ef703d5ab68
Daru <ozziewei@gmail.com>
2024-10-20 23:09:12 +0800

    implements file location in explorer

--- FileSystem/Filesystem.cpp
+++ FileSystem/Filesystem.cpp
@@ -1,6 +1,15 @@
 #include "Utility/FileSystem/Filesystem.h"
+#include "Utility/String/Encoding.h"
 #include "spdlog/spdlog.h"
+#include <exception>
 #include <functional>
+#include <memory>
+#include <string>
+
+#if defined(_WIN32)
+#include <Windows.h>
+#include <shellapi.h>
+#endif
 
 namespace {
 using RemoveFuncTy = std::function<bool(const std::filesystem::path &)>;
@@ -58,6 +67,43 @@ bool removeAll(const PathListTy &path) {
   return removeBatch(path, [](const std::filesystem::path &p) {
     return utility::filesystem::removeAll(p);
   });
+}
+
+bool select(const std::filesystem::path &path) {
+  if (!std::filesystem::exists(path)) {
+    spdlog::error("select: path doesn't exist. {}", path.string());
+    return false;
+  }
+
+#if defined(_WIN32)
+  const wchar_t select[] = L" /select,";
+  const std::u16string filePath{path.u16string()};
+  const size_t buf_size = wcslen(select) + filePath.size() + 4 * sizeof(wchar_t);
+
+  try {
+    std::unique_ptr<wchar_t[]> buf = std::make_unique<wchar_t[]>(buf_size);
+    wchar_t *const buf_ptr = buf.get();
+    _snwprintf_s(buf_ptr, buf_size * sizeof(wchar_t), buf_size, L"%s\"%s\"",
+                 select, (wchar_t *)filePath.c_str());
+
+    // MSDN document says we can cast the HINSTANCE return type to an INT_PTR.
+    // https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutew
+    int retVal = (int)ShellExecuteW(NULL, L"open", L"explorer.exe", buf_ptr,
+                                    NULL, SW_SHOWDEFAULT);
+
+    if (!retVal || retVal <= 32) {
+      spdlog::error("select: ShellExecuteW error with return value {}", retVal);
+      return false;
+    }
+  } catch (const std::exception &e) {
+    spdlog::error("select: {}", e.what());
+    return false;
+  }
+
+  return true;
+#else
+  return false;
+#endif
 }
 } // namespace filesystem
 } // namespace utility
