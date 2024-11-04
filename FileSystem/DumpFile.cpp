ec7b280a9ca6dff61afca2d480e7bbc2c54bcaa2
scania <ozziewei@gmail.com>
2024-11-04 16:16:54 +0800

    move DumpFile to Utility

---
+++ FileSystem/DumpFile.cpp
@@ -0,0 +1,24 @@
+#include "Utility/FileSystem/DumpFile.h"
+#include "spdlog/spdlog.h"
+#include <cassert>
+
+namespace bbdown {
+namespace filesystem {
+bool DumpFile(const std::string &file_name, const std::string &content,
+              const std::ios_base::openmode mode) {
+  assert(!(mode & std::ios_base::in));
+
+  std::ofstream file{file_name, mode};
+  if (!file.is_open()) {
+    spdlog::error("failed to write {}", file_name);
+    return false;
+  }
+
+  file << content;
+  file.flush();
+  file.close();
+
+  return true;
+}
+} // namespace filesystem
+} // namespace bbdown
