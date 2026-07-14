de65ffd3c9df76ec9bb886a308d9801ffa90d453
Daru <ozziewei@gmail.com>
2026-07-14 16:08:58 +0800

    [NFC]Fix include guard

--- include/Utility/DllExport.h
+++ include/Utility/DllExport.h
@@ -1,14 +1,20 @@
-#ifndef DLLEXPORT_H
-#define DLLEXPORT_H
+#ifndef UTILITY_DLLEXPORT_H
+#define UTILITY_DLLEXPORT_H
 
 #ifdef _WIN32
+#ifdef DllExport
+#undef DllExport
+#endif
 #if UTILITY_DLL
 #define DllExport __declspec(dllexport)
 #else
 #define DllExport
 #endif // UTILITY_DLL
 #else
+#ifdef DllExport
+#undef DllExport
+#endif
 #define DllExport
 #endif // _WIN32
 
-#endif // DLLEXPORT_H
+#endif // UTILITY_DLLEXPORT_H
